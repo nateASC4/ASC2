537100276d680140dc57fb0aaeefc0e73406eff1
nateASC4 <nantwiseeds@gmail.com>
2017-07-11 10:00:47 -0700

    first commit

---
+++ nathan-variables/pokemonVariable.js/Pokemon.js
@@ -0,0 +1,10 @@
+var arceus = "name";
+var arceusHP = 6980;
+var arceusNum = 493;
+var arceusType = "Normal";
+var arceusBestMove = "Judgement";
+var arceusLastEvo = "True";
+console.log(arceus);
+// var arceusAttack =
+// car arceusDefense =
+// var arceusSpeed = 
---
+++ nathan-variables/pokemonVariable.js/PokemonArray.js
@@ -0,0 +1,15 @@
+var myPokemonRoster = ["arceus",'Zekrom',"Garchomp","Darkrai","Giratina"]
+var firstToFight = myPokemonRoster[0]
+var secondToFight = myPokemonRoster[1]
+    // myarray= [0]
+for (var i=0 ; i<5; i++){
+    (console.log(myPokemonRoster[i])
+}
+
+for (var i=4 i<5 i--){
+(console.log(myPokemonRoster[i])
+}
+for (var i=4 i< i++){
+    console.length
+}
+
