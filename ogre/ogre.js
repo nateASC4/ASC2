215209e4f71134c7ccceff4c650bcdf5211d3e62
nateASC4 <nantwiseeds@gmail.com>
2017-07-28 17:05:05 -0400

    Completed Html Bootstrap for personal site

---
+++ ogre/ogre.js
@@ -0,0 +1,13 @@
+var playerHP = 100; 
+var playerGold = 100;
+var playerDefeated = 0; 
+
+$(document).ready(){
+
+}
+function ghana(){
+    $("body").append('<h1>Greetings</h1>');
+    $('body').append("HP:" + playerHP +"Gold"+ playerDefeated);
+    
+}
+
